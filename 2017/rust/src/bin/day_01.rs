21ae8729b0258babdd4baeec3e2b1ad363b01563
Kevin Yeh <kevinyeah@utexas.edu>
2017-12-02 00:42:39 -0600

    1-2 lol i spent so much time fighting to compile to try and refactor this and failed bye

--- 2017/rust/src/bin/day_01.rs
+++ 2017/rust/src/bin/day_01.rs
@@ -5,5 +5,6 @@ use std::env;
 
 fn main() {
     let input = env::args().nth(1).unwrap();
-    println!("{}", day_01::part_1::solve_captcha(&input));
+    println!("part 1: {}", day_01::part_1::solve_captcha(&input));
+    println!("part 2: {}", day_01::part_2::solve_captcha(&input));
 }
--- 2017/rust/src/bin/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate aoc;
-
-use aoc::day_01;
-use std::env;
-
-fn main() {
-    let input = env::args().nth(1).unwrap();
-    println!("{}", day_01::part_1::solve_captcha(&input));
-}
