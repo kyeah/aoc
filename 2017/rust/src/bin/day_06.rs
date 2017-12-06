be4a30bfa74f231d2efecf528cb16cfbd9cfac60
Kevin Yeh <kevinyeah@utexas.edu>
2017-12-06 00:18:30 -0600

    some may say cleanup for day 06

--- 2017/rust/src/bin/day_06.rs
+++ 2017/rust/src/bin/day_06.rs
@@ -15,33 +15,35 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut seen = HashSet::new();
-    while !seen.contains(&banks) {
-        seen.insert(banks.clone());
-        banks = redistribute(banks);
-    }
+    let iters = redistribute_until_loop(&mut banks);
+    println!("part 1: {}", iters);
 
-    println!("part 1: {}", seen.len());
-
-    seen.clear();
-    while !seen.contains(&banks) {
-        seen.insert(banks.clone());
-        banks = redistribute(banks);
-    }
-
-    println!("part 2: {}", seen.len());
+    let inner_iters = redistribute_until_loop(&mut banks);
+    println!("part 2: {}", inner_iters);
 }
 
-fn redistribute(banks: Vec<usize>) -> Vec<usize> {
-    let mut bank: Vec<_> = banks.clone();
-
-    // This is a terrible ineffecient way to do this sorry
-    let max_value = banks.iter().max().expect("no values");
-    let pos = banks.iter().position(|val| val == max_value).expect("lol");
-
-    bank[pos] = 0;
-    for offset in 1..max_value+1 {
-        bank[(pos + offset) % banks.len()] += 1;
+fn redistribute_until_loop(banks: &mut Vec<usize>) -> usize {
+    let mut seen = HashSet::new();
+    while !seen.contains(banks) {
+        seen.insert(banks.clone());
+        redistribute(banks);
     }
-    bank
+    seen.len()
+}
+
+fn redistribute(banks: &mut Vec<usize>) {
+    let len = banks.len();
+
+    // this is unnecessarily dense and probably
+    // worse than my initial solution
+    let (pos, max_value) = banks.iter()
+        .enumerate()
+        .max_by_key(|&(pos, item)| (item, -(pos as i32)))
+        .map(|(pos, val)| (pos, val.to_owned()))
+        .expect("no items");
+
+    banks[pos] = 0;
+    for offset in 1..max_value+1 {
+        banks[(pos + offset) % len] += 1;
+    }
 }
