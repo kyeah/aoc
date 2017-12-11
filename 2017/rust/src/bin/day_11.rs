8ea3811e493e9a7cd3fcfca3e4d207b503b6579f
Kevin Yeh <kevinyeah@utexas.edu>
2017-12-11 00:30:43 -0600

    11

---
+++ 2017/rust/src/bin/day_11.rs
@@ -0,0 +1,34 @@
+extern crate aoc;
+extern crate itertools;
+
+use aoc::utils;
+use std::collections::HashMap;
+
+fn main() {
+    let input = utils::arg_or_default(include_str!("../../res/day_11/input.txt"));
+
+    let mut dirs = HashMap::new();
+    for key in vec!["n", "s", "sw", "se", "nw", "ne"] {
+        dirs.entry(key).or_insert(0 as isize);
+    }
+
+    let mut max_dist = 0;
+    for dir in input.split(",") {
+        utils::add(&mut dirs, &dir, 1);
+        max_dist = max(max_dist, distance(&dirs));
+    }
+
+    println!("part 1: {}", distance(&dirs));
+    println!("part 2: {}", max_dist);
+}
+
+fn max(a: isize, b: isize) -> isize {
+    if a > b { a } else { b }
+}
+
+fn distance(dirs: &HashMap<&str, isize>) -> isize {
+    let left  = dirs["nw"] - dirs["se"];
+    let up    = dirs["n"] - dirs["s"];
+    let right = dirs["ne"] - dirs["sw"];
+    up + max(left.abs(), right.abs())
+}
