32fee27b59587badcba6a6145ea54a33d496845d
Kevin Yeh <kevinyeah@utexas.edu>
2018-12-01 21:28:23 -0500

    2018 day 1 wow such tap

---
+++ 2018/day_01.rb
@@ -0,0 +1,23 @@
+require 'set'
+
+def input
+  File.readlines('./res/day_01.txt').map{|x| Integer(x)}
+end
+
+def p1
+  input.sum
+end
+
+def p2
+  sum, seen = 0, Set[0]
+
+  loop do
+    return sum if input.find do |x|
+      sum += x
+      seen.include?(sum).tap{ seen.add(sum) }
+    end
+  end
+end
+
+puts p1
+puts p2
