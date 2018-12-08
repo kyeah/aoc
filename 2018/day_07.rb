ce2f93a3e86d1831b707e03397fb99e4fc506626
Kevin Yeh <kevinyeah@utexas.edu>
2018-12-07 23:39:33 -0500

    day 7 sorry

---
+++ 2018/day_07.rb
@@ -0,0 +1,65 @@
+require 'set'
+
+class Graph
+  attr_accessor :vertices
+
+  def initialize
+    @vertices = Set.new
+  end
+
+  def add_edge(a, b)
+    vA = self.vertices.find{ |v| v.id == a }
+    vB = self.vertices.find{ |v| v.id == b }
+    if !vA
+      vA = Vertex.new(a, Array.new, Array.new)
+      self.vertices.add(vA)
+    end
+    if !vB
+      vB = Vertex.new(b, Array.new, Array.new)
+      self.vertices.add(vB)
+    end    
+    vA.children.push(vB)
+    vB.parents.push(vA)
+  end
+
+  def roots_helper(node)
+    if node.parents.empty?
+      [node]
+    else
+      node.parents.map{|n| roots_helper(n)}.flatten
+    end
+  end
+
+  def roots
+    self.vertices.map{|v| self.roots_helper(v)}.flatten.uniq
+  end
+end
+
+class Vertex < Struct.new(:id, :parents, :children)
+end
+
+def input
+  graph = Graph.new
+  File.readlines('./res/day_07.txt').each do |line|
+    graph.add_edge(line.split[1], line.split[7])
+  end
+
+  done = Array.new
+  queue = graph.roots.sort_by(&:id)
+  p queue.map(&:id)
+  while !queue.empty?
+    idx = queue.find_index{|n| !done.include?(n.id) && n.parents.all?{|p| done.include?(p.id)} }
+    break unless idx
+    node = queue.delete_at(idx)
+    done.push(node.id)
+    queue = (queue + node.children).sort_by(&:id)
+    p queue.map(&:id)
+  end
+  done.join
+end
+
+def p1
+  input
+end
+
+p p1
