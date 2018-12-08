539414317e22be174791a371fc5a8eb61afc33bc
Kevin Yeh <kevinyeah@utexas.edu>
2018-12-08 13:03:48 -0500

    a+

--- 2018/day_08.rb
+++ 2018/day_08.rb
@@ -1,56 +1,55 @@
-class Node < Struct.new(:child_n, :metadata_n, :children, :metadata)
-  attr_accessor :id
+DEBUG = false
+
+class Node < Struct.new(:id, :child_n, :metadata_n, :children, :metadata, :parent)
   def sum
     self.metadata.sum + self.children.map(&:sum).sum
   end
+
+  def value
+    if children.empty?
+      self.metadata.sum
+    else
+      self.metadata.map{|m| children[m - 1]}.compact.map(&:value).sum
+    end
+  end
 end
 
-def read_node(thingies)
+def read_node(id, thingies, parent)
+  id += 1
   child_n = thingies.shift
   metadata_n = thingies.shift
-  Node.new(child_n, metadata_n, Array.new, Array.new)
+  Node.new(id, child_n, metadata_n, Array.new, Array.new, parent)
 end
 
 def input
   id = 0
   thingies = File.read('./res/day_08.txt').split.map(&:to_i)
-  #thingies = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split.map(&:to_i)
-  nodes, nodes_waiting_for_metadata = Array.new, Array.new
-  root = read_node(thingies)
+
+  nodes = Array.new
+  root = read_node(id, thingies, nil)
   nodes.push(root)
 
   while !thingies.empty?
     last_node = nodes.reverse.find{ |n| n.child_n != 0 }
     break if !last_node
 
-    node = read_node(thingies)
-    node.id = id
-    id += 1
-    p ["read_node:", node.id, node.child_n, node.metadata_n]
+    node = read_node(id, thingies, last_node)
+    p ["read_node:", last_node.id, node.id, node.child_n, node.metadata_n] if DEBUG
 
     nodes.push(node)
-
     last_node.children.push(node)
     last_node.child_n -= 1
 
-    if last_node.child_n == 0
-      nodes_waiting_for_metadata.push(last_node)
-    end
-
     if node.child_n == 0
-      nodes_waiting_for_metadata.push(node)
-      mnode = nodes_waiting_for_metadata.last
-      while mnode && mnode.child_n == 0
-        nodes_waiting_for_metadata.pop
+      mnode = node
+      while mnode && mnode.child_n == 0 && mnode.metadata_n > 0
         mnode.metadata = thingies.shift(mnode.metadata_n)
+        p ["read_metadata:", mnode.id, mnode.metadata_n, mnode.metadata] if DEBUG
+
         mnode.metadata_n = 0
-        mnode = nodes_waiting_for_metadata.last
+        mnode = mnode.parent
       end
     end
-  end
-
-  if !nodes_waiting_for_metadata.empty?
-    puts 'uh oh'
   end
 
   root
@@ -60,4 +59,9 @@ def p1
   input.sum
 end
 
+def p2
+  input.value
+end
+
 puts p1
+puts p2
