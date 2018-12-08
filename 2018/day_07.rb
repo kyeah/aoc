ffb31df08fa27ffbf02c8a39beca3b29594f6e4e
Kevin Yeh <kevinyeah@utexas.edu>
2018-12-08 00:04:47 -0500

    day 7 part 2 yeah fine it works okay at least i didn't play smash

--- 2018/day_07.rb
+++ 2018/day_07.rb
@@ -61,4 +61,53 @@ def p1
   done.join
 end
 
+## UHUGUHU
+class Worker < Struct.new(:node, :time_left)
+end
+
+def p2
+  graph = input
+  done = Array.new
+  queue = graph.roots.sort_by(&:id)
+  workers = Array.new(5){ Worker.new(nil, 0) }
+
+  elapsed_time = 0
+  while !queue.empty?
+    while true
+      p queue.map(&:id)
+      idx = queue.find_index do |n|
+        !done.include?(n.id) &&
+          n.parents.all?{|p| done.include?(p.id)} &&
+          workers.none?{|w| w.node&.id == n.id}
+      end
+      available_worker = workers.find{|w| !w.node}
+      if idx && available_worker
+        node = queue.delete_at(idx)
+        available_worker.node = node
+        available_worker.time_left = 60 + (node.id.ord - 64)
+      else
+        break
+      end
+    end
+
+    break if workers.all?{|w| !w.node}
+    step = workers.select(&:node).compact.map(&:time_left).min
+
+    workers.each do |w|
+      next if !w.node
+      w.time_left -= step
+      if w.time_left == 0
+        done.push(w.node.id)
+        queue = (queue + w.node.children).sort_by(&:id)
+        w.node = nil
+      end
+    end
+
+    elapsed_time += step
+  end
+
+  elapsed_time
+end
+
 puts p1
+puts p2
