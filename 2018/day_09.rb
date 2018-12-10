e922d717cabe9f796a1e77df9e709e977a4b8223
Kevin Yeh <kevinyeah@utexas.edu>
2018-12-09 23:39:46 -0500

    make doubly linked list, d9 p2

--- 2018/day_09.rb
+++ 2018/day_09.rb
@@ -1,56 +1,50 @@
-mmax = 71626 * 100
-nmax = 438
-
-circle = Array.new(mmax)
-circle[0] = 0
-
-scores = Hash.new { 0 }
-current = 0
-count = 1
-
-# yeah there's an easier way to do this so what
-def loopdeloop(count, i)
-  if i < 0
-    count - (i.abs % count)
-  elsif i >= count
-    i % count
-  else
-    i
+# Doubly-linked list oh yeaaa
+class Node < Struct.new(:prev, :next, :val)
+  def delete
+    self.prev.next = self.next
+    self.next.prev = self.prev
+    self
   end
 end
 
-def pcircle(circle, current)
-  clone = circle.compact.clone
-  clone[current] = "(#{clone[current]})"
-  puts clone.join(" ")
-end
+def have_fun(mmax, nmax)
+  head = Node.new(nil, nil, 0)
+  head.prev = head
+  head.next = head
 
-def insert_idx(i1, i2, index)
-  if i1 === i2
-    i1 + 1
-  elsif (i1 - i2).abs > 1
-    [i1, i2].max + 1
-  else
-    [i1, i2].max
+  scores = Hash.new { 0 }
+  current = head
+
+  def loopdeloop(current, shift)
+    node = current
+    shift.abs.times do
+      node = node.prev if shift < 0
+      node = node.next if shift > 0
+    end
+    node
   end
-end
 
-(1..mmax).each do |m|
-  puts m if m % 71626 === 0
-  if m % 23 === 0
-    scores[m % nmax] += m
-    current = index = loopdeloop(count, current - 7)
-    scores[m % nmax] += circle.delete_at(index)
-    count -= 1
-  else
-    i1 = loopdeloop(count, current + 1)
-    i2 = loopdeloop(count, current + 2)
-    puts i1, i2
-    current = index = insert_idx(i1, i2, index)
-    circle[index] = m
-    count += 1
+  iter = 0
+  (1..mmax).each do |m|
+    puts "#{iter += 1}/100" if m % 71626 === 0
+
+    if m % 23 === 0
+      scores[m % nmax] += m
+      deleted_node = loopdeloop(current, -7).delete
+      scores[m % nmax] += deleted_node.val
+      current = deleted_node.next
+    else
+      n1 = loopdeloop(current, 1)
+      n2 = loopdeloop(current, 2)
+      n1, n2 = n2, n1 if n2.next.val === n1.val
+
+      node = Node.new(n1, n2, m)
+      n1.next = n2.prev = current = node
+    end
   end
-  pcircle(circle, current)
+
+  scores.max_by{|k,v| v}[1]
 end
 
-puts scores.max_by{|k,v| v}
+puts have_fun(71626, 438)
+puts have_fun(7162600, 438)
